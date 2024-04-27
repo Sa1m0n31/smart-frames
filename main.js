719b5d49bc3aefbdf1e63a2d4e771201079c1b60
Szymon Burak <sajmon0031@gmail.com>
2024-04-27 12:03:01 +0200

    Add homepage mobile version

--- main.js
+++ main.js
@@ -20,3 +20,25 @@ const selectTab = (i) => {
         }
     });
 }
+
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
+
+const openMenu = () => {
+    mobileMenu.style.transform = 'scaleX(1)';
+    setTimeout(() => {
+        mobileMenuChildren.forEach((item) => {
+            item.style.opacity = '1';
+        });
+    }, 200);
+}
+
+const closeMenu = () => {
+    mobileMenuChildren.forEach((item) => {
+        item.style.opacity = '0';
+    });
+    setTimeout(() => {
+        mobileMenu.style.transform = 'scaleX(0)';
+    }, 200);
+}
+
