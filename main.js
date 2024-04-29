0a3658f85f362a36a40a74786236359a6f329252
Szymon Burak <sajmon0031@gmail.com>
2024-04-29 23:07:05 +0200

    Add subpages

--- main.js
+++ main.js
@@ -21,6 +21,34 @@ const selectTab = (i) => {
     });
 }
 
+const allMobilityVideos = Array.from(document.querySelectorAll('.mobility--video'));
+const allMobilityHeaders = Array.from(document.querySelectorAll('.mobility--header'));
+const allMobilityDescriptions = Array.from(document.querySelectorAll('.mobility--description'));
+const allMobilityDots = Array.from(document.querySelectorAll('.mobility--dot'));
+
+const selectMobilityVideo = (index) => {
+    let i = 0;
+
+    for(const item of allMobilityVideos) {
+        if(index === i) {
+            allMobilityVideos[i].style.display = 'block';
+            allMobilityHeaders[i].style.color = 'white';
+            allMobilityDescriptions[i].style.display = 'block';
+            allMobilityDots[i].style.background = '#D3F103';
+        }
+        else {
+            allMobilityVideos[i].style.display = 'none';
+            allMobilityHeaders[i].style.color = '#616161';
+            allMobilityDescriptions[i].style.display = 'none';
+            allMobilityDots[i].style.background = '#616161';
+        }
+
+        i++;
+    }
+}
+
+selectMobilityVideo(0);
+
 const mobileMenu = document.querySelector('.mobileMenu');
 const mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
 
@@ -42,3 +70,31 @@ const closeMenu = () => {
     }, 200);
 }
 
+// Portfolio
+const allPortfolioButtons = Array.from(document.querySelectorAll('.portfolioBtn'));
+const allPortfolioItems = Array.from(document.querySelectorAll('.portfolio'));
+
+const selectPortfolio = (n) => {
+    let i = 0;
+
+    for(const item of allPortfolioButtons) {
+        if(i === n) {
+            item.style.background = '#D3F103';
+        }
+        else {
+            item.style.background = 'transparent';
+        }
+
+        i++;
+    }
+
+    allPortfolioItems.forEach((item) => {
+        item.style.display = 'none';
+    });
+
+    setTimeout(() => {
+        Array.from(document.querySelectorAll(`.portfolio--${n}`)).forEach((item) => {
+            item.style.display = 'block';
+        });
+    }, 200);
+}
